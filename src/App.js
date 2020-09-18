a3d69a0d7240c8b740773b6fdbf1a000de723078
Alexander Kotov <alexanderko@interlink-ua.com>
2020-09-18 15:09:42 +0300

    Add class based time

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Timer } from './Timer';
 
 class App extends Component {
 
@@ -24,6 +25,8 @@ class App extends Component {
         <p>Counter: {this.state.counter}</p>
         <button onClick={() => this.setState({counter: this.state.counter + 1})}>Add</button>
         <p>Seconds since start: {this.state.seconds}</p>
+        <Timer name={'Torn off gas'} duration={5 * 60}/>
+        <Timer name={'Workout'} duration={15 * 60}/>
       </div>
     );
   }
