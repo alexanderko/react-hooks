59147390d52413356eb2cc1c65d7bba79aede7db
Alexander Kotov <alexanderko@interlink-ua.com>
2020-09-18 15:50:54 +0300

    Apply useState hook for App component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Timer } from './Timer';
 
-class App extends Component {
+function useCounter(start = 0) {
+  const [counter, setCounter] = useState(start);
+  return [counter, () => { setCounter(counter + 1) }]
+}
 
-  constructor() {
-    super();
-    this.state = { 
-      counter: 0,
-      seconds: 0,
-      timers: [
-        {
-          name: 'Torn off gas',
-          duration: 5 * 60
-        },
-        {
-          name: 'Workout',
-          duration: 15 * 60
-        },
-      ]
+function App() {
+  const [counter, inc] = useCounter();
+  const [timers, setTimers] = useState([
+    {
+      name: 'Torn off gas',
+      duration: 5 * 60
+    },
+    {
+      name: 'Workout',
+      duration: 15 * 60
     }
-    this.removeTimer = this.removeTimer.bind(this);
-  }
+  ]);
 
-  componentDidMount() {
-    setInterval(() => {
-      this.tick()
-    }, 1000);
-  }
+  return (
+    <div>
+      <p>Counter: {counter}</p>
+      <button onClick={inc}>Add</button>
+      {
+        timers.map(
+          timer => <Timer key={timer.name} name={timer.name} duration={timer.duration}/>
+        )
+      }
 
-  tick() {
-    this.setState({seconds: this.state.seconds + 1})
-  }
-
-  removeTimer() {
-    const { timers } = this.state;
-    timers.pop();
-    this.setState({ timers });
-  }
-
-  render() {
-    return (
-      <div>
-        <p>Counter: {this.state.counter}</p>
-        <button onClick={() => this.setState({counter: this.state.counter + 1})}>Add</button>
-        <p>Seconds since start: {this.state.seconds}</p>
-        {
-          this.state.timers.map(
-            timer => <Timer key={timer.name} name={timer.name} duration={timer.duration}/>
-          )
-        }
-
-        <button onClick={this.removeTimer}>Remove timer</button>
-      </div>
-    );
-  }
+      <button onClick={() => {setTimers(timers.slice(1));}}>Remove timer</button>
+    </div>
+  );
 }
 
 export default App;
